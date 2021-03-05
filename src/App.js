d117836808f17b7f0aea56b246ccc682a4b788fe
Marco Cappa <marcocappa85@gmail.com>
2021-03-05 23:52:36 +0100

    React, webpack, babel and prettier setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import card from './images/card.jpeg';
+
+const App = () => (
+  <div>
+    <h1>Hello React</h1>
+    <img src={card} width={300} />
+    <p>this is a test</p>
+  </div>
+);
+
+export default App;
